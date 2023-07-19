43659a7a3280986747bbeebfeefe2279811fcdd4
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2023-07-20 01:01:55 +0400

    :feet: include working demo viz (nsfw)

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import p5 from "p5";
 
 const sketch = (p: p5) => {
   p.setup = () => {
-
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.background(35, 57, 68);
   };
 
   p.draw = () => {
-  
+    p.rect(30, 30, 70, 70);
+    p.fill(p.random(0, 255));
   };
 };
 
